b0f1698f7c3eeeed25b035758b35720e5aad6ec9
pythonstrup <pythonstrup@gmail.com>
2023-08-04 00:07:29 +0900

    ApiResponse 추가

---
+++ src/main/java/com/onebyte/springboilerplate/dto/ApiResponse.java
@@ -0,0 +1,16 @@
+package com.onebyte.springboilerplate.dto;
+
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class ApiResponse<T> {
+
+  T data;
+}
