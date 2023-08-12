87d35c694ce9bdb5e6b8d5837afdc8fcd5d3a232
pythonstrup <pythonstrup@gmail.com>
2023-08-12 16:14:33 +0900

    GlobalExceptionHandler

---
+++ src/main/java/com/onebyte/springboilerplate/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.onebyte.springboilerplate.common.exception;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+  @ExceptionHandler(RuntimeException.class)
+  public ResponseEntity<CustomErrorResponse> handleRuntimeError(final RuntimeException e) {
+    log.error("Uncontrolled Exception ", e);
+    return makeResponseEntity(ExceptionName.RUNTIME_ERROR.getName(), e.getMessage(), HttpStatus.BAD_REQUEST);
+  }
+
+  private ResponseEntity<CustomErrorResponse> makeResponseEntity(
+      String name,
+      String message,
+      HttpStatus httpStatus) {
+    CustomErrorResponse response = CustomErrorResponse.builder()
+        .name(name)
+        .message(message)
+        .build();
+    return new ResponseEntity<>(response, httpStatus);
+  }
+}
