55ebbde2b7ff091dcd182da5ee1e3369c08159ad
pythonstrup <pythonstrup@gmail.com>
2023-08-20 20:14:08 +0900

    Spring Security & OAuth 적용

--- src/main/java/com/onebyte/springboilerplate/common/config/SecurityConfig.java
+++ src/main/java/com/onebyte/springboilerplate/common/config/SecurityConfig.java
@@ -2,11 +2,12 @@ package com.onebyte.springboilerplate.common.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
+//@EnableWebSecurity(debug = true) // 디버그 모드
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
@@ -14,8 +15,19 @@ public class SecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http
-        .authorizeHttpRequests((auth) -> auth.anyRequest().authenticated())
-        .httpBasic(Customizer.withDefaults());
+        .csrf(AbstractHttpConfigurer::disable)
+        .formLogin(AbstractHttpConfigurer::disable)
+        .httpBasic(AbstractHttpConfigurer::disable)
+
+        // authorization
+        .authorizeHttpRequests((auth) -> auth
+            .requestMatchers("/oauth2/**", "/login/**").permitAll()
+            .anyRequest().authenticated()
+        )
+        // OAuth2
+        .oauth2Login(oauth -> oauth
+            .defaultSuccessUrl("/")
+        );
     return http.build();
   }
 }
