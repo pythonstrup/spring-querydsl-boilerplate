75d29a1707d3d3d25576fed7410cd552a01e88a6
pythonstrup <pythonstrup@gmail.com>
2023-08-22 22:50:25 +0900

    UserSignInRequest

---
+++ src/main/java/com/onebyte/springboilerplate/domain/dto/user/UserSignInRequest.java
@@ -0,0 +1,28 @@
+package com.onebyte.springboilerplate.domain.dto.user;
+
+import com.onebyte.springboilerplate.domain.entity.User;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Getter
+@Builder
+public class UserSignInRequest {
+  private String username;
+  private String password;
+  private String email;
+  private Integer age;
+
+  public User toEntity() {
+    return User.builder()
+        .username(username)
+        .password(password)
+        .email(email)
+        .age(age)
+        .build();
+  }
+}
