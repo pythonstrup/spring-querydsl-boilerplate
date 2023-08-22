2569ead0d7efe753a7c80e2ba0015a157b470bf1
pythonstrup <pythonstrup@gmail.com>
2023-08-22 20:07:07 +0900

    OAuth FailureHandler

---
+++ src/main/java/com/onebyte/springboilerplate/service/oauth/OAuthLoginFailureHandler.java
@@ -0,0 +1,23 @@
+package com.onebyte.springboilerplate.service.oauth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class OAuthLoginFailureHandler extends SimpleUrlAuthenticationFailureHandler {
+
+  @Override
+  public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
+      AuthenticationException exception) throws IOException, ServletException {
+
+    log.error("Login Fail: {}", exception.getMessage());
+    super.onAuthenticationFailure(request, response, exception);
+  }
+}
