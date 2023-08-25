d07e582010fd9c3f8cafdc2cab80110ba2961be7
pythonstrup <pythonstrup@gmail.com>
2023-08-25 21:03:54 +0900

    SessionAuthenticationFailureHandler

---
+++ src/main/java/com/onebyte/springboilerplate/common/service/auth/session/SessionAuthenticationFailureHandler.java
@@ -0,0 +1,21 @@
+package com.onebyte.springboilerplate.common.service.auth.session;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SessionAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+  @Override
+  public void onAuthenticationFailure(
+      HttpServletRequest request,
+      HttpServletResponse response,
+      AuthenticationException exception) {
+
+    response.setStatus(HttpStatus.UNAUTHORIZED.value());
+  }
+}
