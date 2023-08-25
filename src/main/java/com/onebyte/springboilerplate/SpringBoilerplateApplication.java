4d01a8be031a71acd5ff3eb01ad2cd34078d1ccf
pythonstrup <pythonstrup@gmail.com>
2023-08-25 21:04:43 +0900

    ObjectMapper Bean 등록

--- src/main/java/com/onebyte/springboilerplate/SpringBoilerplateApplication.java
+++ src/main/java/com/onebyte/springboilerplate/SpringBoilerplateApplication.java
@@ -1,7 +1,9 @@
 package com.onebyte.springboilerplate;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBoilerplateApplication {
@@ -10,4 +12,8 @@ public class SpringBoilerplateApplication {
     SpringApplication.run(SpringBoilerplateApplication.class, args);
   }
 
+  @Bean
+  public ObjectMapper objectMapper() {
+    return new ObjectMapper();
+  }
 }
