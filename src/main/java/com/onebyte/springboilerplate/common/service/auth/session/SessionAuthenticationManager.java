273b1a69ab0c3266d82d724f7cdfb0927e231e5e
pythonstrup <pythonstrup@gmail.com>
2023-08-25 21:04:20 +0900

    SessionAuthenticationManager

---
+++ src/main/java/com/onebyte/springboilerplate/common/service/auth/session/SessionAuthenticationManager.java
@@ -0,0 +1,37 @@
+package com.onebyte.springboilerplate.common.service.auth.session;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Transactional
+@Component
+@RequiredArgsConstructor
+public class SessionAuthenticationManager implements AuthenticationManager {
+
+  private final UserDetailsService userDetailsService;
+  private final PasswordEncoder passwordEncoder;
+  private final ObjectMapper objectMapper;
+
+  @Override
+  public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+    UserDetails userDetails = userDetailsService.loadUserByUsername(authentication.getName());
+
+    String requestPassword = authentication.getCredentials().toString();
+    if (!passwordEncoder.matches(requestPassword, userDetails.getPassword())) {
+      throw new BadCredentialsException("Not Match Password");
+    }
+
+    return new UsernamePasswordAuthenticationToken(userDetails.getUsername(), userDetails.getPassword(),
+        userDetails.getAuthorities());
+  }
+}
