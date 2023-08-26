e93d64bc149f0972f1870e76c30b4f98f51408f1
pythonstrup <pythonstrup@gmail.com>
2023-08-26 15:15:09 +0900

    UserLoginRequest Dto

---
+++ src/main/java/com/onebyte/springboilerplate/domain/dto/user/UserLoginRequest.java
@@ -0,0 +1,14 @@
+package com.onebyte.springboilerplate.domain.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserLoginRequest {
+
+  private String username;
+  private String password;
+}
