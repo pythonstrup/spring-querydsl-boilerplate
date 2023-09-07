472c0f4467511e1219d4f685fe720624d638742a
pythonstrup <pythonstrup@gmail.com>
2023-09-07 21:20:44 +0900

    Cors 설정 추가

--- src/main/java/com/onebyte/springboilerplate/common/config/SecurityConfig.java
+++ src/main/java/com/onebyte/springboilerplate/common/config/SecurityConfig.java
@@ -17,6 +17,7 @@ import com.onebyte.springboilerplate.common.service.oauth.OAuthLoginSuccessHandl
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -24,6 +25,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 //@EnableWebSecurity(debug = true) // 디버그 모드
 @EnableWebSecurity
@@ -47,6 +51,7 @@ public class SecurityConfig {
         .csrf(AbstractHttpConfigurer::disable)
         .formLogin(AbstractHttpConfigurer::disable)
         .httpBasic(AbstractHttpConfigurer::disable)
+        .cors(Customizer.withDefaults())
 
         /* Sessions
         .addFilterBefore(
@@ -92,6 +97,20 @@ public class SecurityConfig {
     return http.build();
   }
 
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+
+    configuration.addAllowedOriginPattern("*");
+    configuration.addAllowedMethod("*");
+    configuration.addAllowedHeader("*");
+    configuration.setAllowCredentials(true);
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 
   /**
    * Jwt Auth
